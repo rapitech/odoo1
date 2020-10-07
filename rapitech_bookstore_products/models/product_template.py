a03b59458e80a4c2698ec92bad973166d43313b8
root <jcavalier@rapi.tech>
2020-10-07 17:48:08 -0500

    MOD rapitech_bookstore_products

--- rapitech_bookstore_products/models/product_template.py
+++ rapitech_bookstore_products/models/product_template.py
@@ -2,30 +2,31 @@
 import logging
 
 from odoo import api, fields, models, tools
+from datetime import datetime
 
 _logger = logging.getLogger(__name__)
+
+def year_now():
+    return datetime.now().year
 
 YEAR_SELECTION = [(str(year), year) for year in range(1450, year_now())]
 
 class BookFormat(models.Model):
-	_inherit = 'book.format'
+    _name = 'book.format'
 
-	name = fields.Char(string='Formato')
+    name = fields.Char(string='Formato')
 
 
 class BookAuthor(models.Model):
-	_inherit = 'book.Author'
+    _name = 'book.author'
 
-	name = fields.Char(string='Autor')
-	country_id = fields.Many2one(comodel_name='res.country', string='País')
-    image = fields.Binary("Logo", attachment=True,
-        help="This field holds the image used as logo for the brand, limited to 1024x1024px.")
-    image_medium = fields.Binary("Medium-sized image", attachment=True,
-        help="Medium-sized logo of the brand. It is automatically "
+    name = fields.Char(string='Autor')
+    country_id = fields.Many2one(comodel_name='res.country', string='País')
+    image = fields.Binary(string="Logo", attachment=True, help="This field holds the image used as logo for the brand, limited to 1024x1024px.")
+    image_medium = fields.Binary(string="Medium-sized image", attachment=True, help="Medium-sized logo of the brand. It is automatically "
              "resized as a 128x128px image, with aspect ratio preserved. "
              "Use this field in form views or some kanban views.")
-    image_small = fields.Binary("Small-sized image", attachment=True,
-        help="Small-sized logo of the brand. It is automatically "
+    image_small = fields.Binary(string="Small-sized image", attachment=True, help="Small-sized logo of the brand. It is automatically "
              "resized as a 64x64px image, with aspect ratio preserved. "
              "Use this field anywhere a small image is required.")
 
@@ -41,21 +42,21 @@ class BookAuthor(models.Model):
         return super(BookAuthor, self).write(vals)
 
 class BookPublisher(models.Model):
-	_inherit = 'book.publisher'
+    _name = 'book.publisher'
 
-	name = fields.Char(string='Editorial')
+    name = fields.Char(string='Editorial')
 
 class FillingType(models.Model):
-	_inherit = 'filling.type'
+    _name = 'filling.type'
 
-	name = fields.Char(string='Tipo de estampe')
+    name = fields.Char(string='Tipo de estampe')
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     isbn_13 = fields.Integer(string='ISBN-13')
     edition_number = fields.Integer(string='N° de edición')
-    year = fields.Selection(selection=YEAR_SELECTION.reverse(), string='Año')
+    year = fields.Selection(selection=YEAR_SELECTION, string='Año')
     book_format = fields.Many2one(comodel_name='book.format', string='Formato')
     author = fields.Many2one(comodel_name='book.author', string='Autor')
     editorial = fields.Many2one(comodel_name='book.publisher', string='Editorial')
